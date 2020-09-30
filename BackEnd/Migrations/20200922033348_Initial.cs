76e99801f5d8bd9b09e5579174846b40cca51cd3
DESKTOP-O5BLINM\Admin <rflorentino@wepsys.net>
2020-09-29 23:56:34 -0400

    All changes

---
+++ BackEnd/Migrations/20200922033348_Initial.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BackEnd.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tarea",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "Varchar(100)", nullable: true),
+                    Finalizada = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tarea", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tarea");
+        }
+    }
+}
---
+++ BackEnd/Models/Tarea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEnd.Models
+{
+    public class Tarea
+    {
+        [Key]
+        public int Id { get; set; }
+        [Column(TypeName = "Varchar(100)")]
+        public string Nombre { get; set; }
+        public bool Finalizada { get; set; }
+    }
+}
